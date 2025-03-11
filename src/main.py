8d38926b6583c852751d8cbf22ce8dd936c6d427
Eric García <garciagomezeric10@gmail.com>
2025-03-11 15:49:00 +0100

    update endpoints get_games_by_condition

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, status
 from fastapi.exceptions import HTTPException
 
-from src.database.fake_games import get_game_by_name, get_games_by_id, is_game_id, get_games_by_genre
+from src.database.fake_games import get_game_by_name, get_games_by_id, is_game_id, get_games_by_genre, get_games_genres, get_games_publisher, get_games_platform, get_games_years, get_games_by_publisher, get_games_by_platform, get_games_by_genre, get_games_by_year
 
 app = FastAPI()
 
@@ -26,6 +26,48 @@ def get_game(id: int):
 def get_name(name: str):
     return get_game_by_name(name)
 
+
 @app.get("/games/genre/{genre}")
 def get_genre(genre: str):
     return get_games_by_genre(genre)
+
+
+@app.get("/games/genre/")
+def get_genres():
+    return get_games_genres()
+
+
+@app.get("/games/publisher/")
+def get_publishers():
+    return get_games_publisher()
+
+
+@app.get("/games/platform/")
+def get_platforms():
+    return get_games_platform()
+
+
+@app.get("/games/year/")
+def get_years():
+    return get_games_years()
+
+
+@app.get("/games/publisher/{publisher}")
+def get_name_games_by_publisher(publisher:str):
+    return get_games_by_publisher(publisher)
+
+
+@app.get("/games/platform/{platform}")
+def get_name_games_by_platform(platform:str):
+    return get_games_by_platform(platform)
+
+
+@app.get("/games/genre/{genre}")
+def get_name_games_by_genre(genre:str):
+    return get_games_by_genre(genre)
+
+
+@app.get("/games/year/{year}")
+def get_name_games_by_year(year:int):
+    return get_games_by_year(year)
+
