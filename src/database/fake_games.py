da42a571fe55d9d2ae74fe172f182c5723a39977
Eric García <garciagomezeric10@gmail.com>
2025-03-11 15:48:24 +0100

    update fake_games get_games_by condition

--- src/database/fake_games.py
+++ src/database/fake_games.py
@@ -26,3 +26,39 @@ def get_games_by_genre(genre: str):
     query= f"Genre == '{genre}'"
     result= database_data.query(query).to_dict(orient="records")[0:100]
     return result
+
+def get_games_genres():
+    result = database_data['Genre'].drop_duplicates().tolist()
+    return result
+
+def get_games_publisher():
+    result = database_data['Publisher'].drop_duplicates().tolist()[0:33]
+    return result
+
+def get_games_platform():
+    result = database_data['Platform'].drop_duplicates().tolist()
+    return result
+
+def get_games_years():
+    result= database_data['Year'].drop_duplicates().tolist()[0:30]
+    return result
+
+def get_games_by_publisher(publisher:str):
+    query= f"Publisher=='{publisher}'"
+    result=database_data.query(query)['Name'].drop_duplicates().tolist()[0:100]
+    return result
+
+def get_games_by_platform(platform:str):
+    query= f"Platform=='{platform}'"
+    result=database_data.query(query)['Name'].drop_duplicates().tolist()[0:100]
+    return result
+
+def get_games_by_genre(genre:str):
+    query= f"Genre=='{genre}'"
+    result=database_data.query(query)['Name'].drop_duplicates().tolist()[0:100]
+    return result
+
+def get_games_by_year(year:int):
+    query= f"Year=={year}"
+    result=database_data.query(query)['Name'].drop_duplicates().tolist()[0:100]
+    return result
