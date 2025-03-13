27d7732eb1039c2f6e028ff7e922c814a9f9a264
Eric García <garciagomezeric10@gmail.com>
2025-03-13 12:50:24 +0100

    update endpoints by limit and fix errors

--- src/main.py
+++ src/main.py
@@ -27,11 +27,6 @@ def get_name(name: str):
     return get_game_by_name(name)
 
 
-@app.get("/games/genre/{genre}")
-def get_genre(genre: str):
-    return get_games_by_genre(genre)
-
-
 @app.get("/games/genre/")
 def get_genres():
     return get_games_genres()
@@ -53,35 +48,35 @@ def get_years():
 
 
 @app.get("/games/publisher/{publisher}")
-def get_name_games_by_publisher(publisher:str):
-    return get_games_by_publisher(publisher)
+def get_name_games_by_publisher(publisher:str, limit:int=10):
+    return get_games_by_publisher(publisher, limit)
 
 
 @app.get("/games/platform/{platform}")
-def get_name_games_by_platform(platform:str):
-    return get_games_by_platform(platform)
+def get_name_games_by_platform(platform:str, limit:int=10):
+    return get_games_by_platform(platform, limit)
 
 
 @app.get("/games/genre/{genre}")
-def get_name_games_by_genre(genre:str):
-    return get_games_by_genre(genre)
+def get_name_games_by_genre(genre:str, limit:int=10):
+    return get_games_by_genre(genre, limit)
 
 
 @app.get("/games/year/{year}")
-def get_name_games_by_year(year:int):
-    return get_games_by_year(year)
+def get_name_games_by_year(year:int, limit:int=10):
+    return get_games_by_year(year, limit)
 
 
 @app.get("/games/year/{year_1}/{year_2}")
-def get_name_games_between_year(year_1: int, year_2: int):
-    return get_games_between_years(year_1, year_2)
+def get_name_games_between_year(year_1: int, year_2: int, limit:int=10):
+    return get_games_between_years(year_1, year_2, limit)
 
 
 @app.get("/games/eu_sales/{eu_sales_1}/{eu_sales_2}")
-def get_name_games_between_eu_sales(eu_sales_1: float, eu_sales_2: float):
-    return get_games_between_eu_sales(eu_sales_1, eu_sales_2)
+def get_name_games_between_eu_sales(eu_sales_1: float, eu_sales_2: float, limit:int=10):
+    return get_games_between_eu_sales(eu_sales_1, eu_sales_2, limit)
 
 
 @app.get("/games/na_sales/{na_sales_1}/{na_sales_2}")
-def get_name_games_between_na_sales(na_sales_1: float, na_sales_2: float):
-    return get_games_between_na_sales(na_sales_1, na_sales_2)
+def get_name_games_between_na_sales(na_sales_1: float, na_sales_2: float, limit:int=10):
+    return get_games_between_na_sales(na_sales_1, na_sales_2, limit)
