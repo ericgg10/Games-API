5175fe6b9e8ccbf93a074855b904679ed66dd4ef
Eric García <garciagomezeric10@gmail.com>
2025-03-13 12:50:00 +0100

    update fake_games by limit and fix errors

--- src/database/fake_games.py
+++ src/database/fake_games.py
@@ -22,11 +22,6 @@ def get_game_by_name(name: str):
     return result
 
 
-def get_games_by_genre(genre: str):
-    query= f"Genre == '{genre}'"
-    result= database_data.query(query).to_dict(orient="records")[0:100]
-    return result
-
 def get_games_genres():
     result = database_data['Genre'].drop_duplicates().tolist()
     return result
@@ -43,37 +38,37 @@ def get_games_years():
     result= database_data['Year'].drop_duplicates().tolist()[0:30]
     return result
 
-def get_games_by_publisher(publisher:str):
+def get_games_by_publisher(publisher:str, limit: int):
     query= f"Publisher=='{publisher}'"
-    result=database_data.query(query)['Name'].drop_duplicates().tolist()[0:100]
+    result=database_data.query(query)['Name'].drop_duplicates().tolist()[0:limit]
     return result
 
-def get_games_by_platform(platform:str):
+def get_games_by_platform(platform:str, limit: int):
     query= f"Platform=='{platform}'"
-    result=database_data.query(query)['Name'].drop_duplicates().tolist()[0:100]
+    result=database_data.query(query)['Name'].drop_duplicates().tolist()[0:limit]
     return result
 
-def get_games_by_genre(genre:str):
+def get_games_by_genre(genre:str, limit: int):
     query= f"Genre=='{genre}'"
-    result=database_data.query(query)['Name'].drop_duplicates().tolist()[0:100]
+    result=database_data.query(query)['Name'].drop_duplicates().tolist()[0:limit]
     return result
 
-def get_games_by_year(year:int):
+def get_games_by_year(year:int, limit: int):
     query= f"Year=={year}"
-    result=database_data.query(query)['Name'].drop_duplicates().tolist()[0:100]
+    result=database_data.query(query)['Name'].drop_duplicates().tolist()[0:limit]
     return result
 
-def get_games_between_years(year_1: int, year_2: int ):
+def get_games_between_years(year_1: int, year_2: int, limit: int ):
     query= f"Year>={year_1} and Year<={year_2} or Year>={year_2} and Year<={year_1}"
-    result=database_data.query(query)['Name'].drop_duplicates().tolist()[0:100]
+    result=database_data.query(query)['Name'].drop_duplicates().tolist()[0:limit]
     return result
 
-def get_games_between_eu_sales(eu_sales_1: float, eu_sales_2: float ):
+def get_games_between_eu_sales(eu_sales_1: float, eu_sales_2: float, limit: int ):
     query= f"EU_Sales>={eu_sales_1} and EU_Sales<={eu_sales_2} or EU_Sales>={eu_sales_2} and EU_Sales<={eu_sales_1}"
-    result=database_data.query(query)['Name'].drop_duplicates().tolist()[0:100]
+    result=database_data.query(query)['Name'].drop_duplicates().tolist()[0:limit]
     return result
 
-def get_games_between_na_sales(na_sales_1: float, na_sales_2: float):
+def get_games_between_na_sales(na_sales_1: float, na_sales_2: float, limit: int):
     query= f"NA_Sales>={na_sales_1} and NA_Sales<={na_sales_2} or NA_Sales>={na_sales_2} and NA_Sales<={na_sales_1}"
-    result=database_data.query(query)['Name'].drop_duplicates().tolist()[0:100]
+    result=database_data.query(query)['Name'].drop_duplicates().tolist()[0:limit]
     return result
