e63de4cedb0c565c6b7f9e00bec31d219d7a81e6
Eric García <garciagomezeric10@gmail.com>
2025-04-07 21:06:13 +0200

    update createdb get games field and between

--- scripts/create_db.py
+++ scripts/create_db.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pandas as pd
 from sqlmodel import Session, SQLModel, select
 
-from src.database import engine, platform_database
+from src.database import engine, platform_db
 from src.models.game_model import Game
 from src.models.game_sales_model import GameSales
 from src.models.genre_model import Genre
@@ -84,16 +84,57 @@ def update_game_year_by_id(game_id, year):
         session.commit()
 
 
+def get_games_field(table, column):
+    with Session(engine) as session:
+        query = select(table)
+        result = session.exec(query)
+        nombres = []
+        for column in result:
+            nombres.append(column.name)
+        print(nombres)
+
+
+def get_games_between(column, field_1, field_2):
+    with Session(engine) as session:
+        query = (
+            select(Game.name)
+            .join(GameSales, Game.gamesales_id == GameSales.id)
+            .where(column >= field_1)
+            .where(column <= field_2)
+        )
+        result = session.exec(query)
+        print(result.all())
+
+
 # -----MAIN-------
 def main():
     if not Path("databasegames.db").exists():
         create_tables()
         insert_values()
     execute_querys()
+    print("---Get Games Field---")
+    print("GENRE")
+    get_games_field(Genre, "name")
+    print("PUBLISHER")
+    get_games_field(Publisher, "name")
+    print("PLATFORM")
+    get_games_field(Platform, "name")
+    print("--------------")
+    print("---Get Games Between---")
+    print("NA_SALES")
+    get_games_between(GameSales.Na_sales, 4.99, 6.6)
+    print("EU_SALES")
+    get_games_between(GameSales.Eu_sales, 4.99, 6.6)
+    print("JP_SALES")
+    get_games_between(GameSales.Jp_sales, 4.99, 6.6)
+    print("OTHER_SALES")
+    get_games_between(GameSales.Other_sales, 0.77, 3.31)
+    print("GLOBAL")
+    get_games_between(GameSales.Global_sales, 4.99, 6.6)
     with Session(engine) as session:
-        platform_database.get_platform_by_name(session, name="Wii")
-        # platform_database.create_platform(session, "Prueba")
-        platform_database.delete_platform_by_id(session, 33)
+        platform_db.get_platform_by_name(session, name="Wii")
+    # platform_database.create_platform(session, "Prueba")
+    # platform_db.delete_platform_by_id(session, 33)
 
 
 if __name__ == "__main__":
