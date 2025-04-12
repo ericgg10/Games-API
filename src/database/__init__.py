a7b866764b2c60981ec49195575edc790ba92324
Eric García <garciagomezeric10@gmail.com>
2025-04-12 19:54:58 +0200

    add database dependencies

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,3 +1,14 @@
-from sqlmodel import create_engine
+from typing import Annotated
+
+from fastapi import Depends
+from sqlmodel import Session, create_engine
 
 engine = create_engine("sqlite:///databasegames.db")  # noqa: F401
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+db_session = Annotated[Session, Depends(get_session)]
