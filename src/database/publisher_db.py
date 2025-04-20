195b03711c5e851d7923593e9eb0fc3c185d32c4
Eric García <garciagomezeric10@gmail.com>
2025-04-20 13:36:23 +0200

    add publisher_db create_publisher and delete_publisher

---
+++ src/database/publisher_db.py
@@ -0,0 +1,20 @@
+from sqlmodel import Session, SQLModel, select
+
+from src.models.publisher_model import Publisher
+
+
+# CREATE PUBLISHER
+def create_publisher(db: Session, game_info: Publisher):
+    db.add(game_info)
+    db.commit()
+    db.refresh(game_info)
+    return game_info
+
+
+# DELETE PUBLISHER
+def delete_publisher_by_id(db: Session, publisher_id):
+    query = select(Publisher).where(Publisher.id == publisher_id)
+    result = db.exec(query).first()
+    db.delete(result)
+    db.commit()
+    return result
--- src/routes/publisher.py
+++ src/routes/publisher.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
-from src.database import db_session, games_db
+from src.database import db_session, games_db, publisher_db
 from src.models.publisher_model import Publisher
 
 router = APIRouter(prefix="/publisher", tags=["Publisher"])
@@ -9,3 +9,15 @@ router = APIRouter(prefix="/publisher", tags=["Publisher"])
 @router.get("/")
 def get_publishers(db: db_session):
     return games_db.get_games_field(db, Publisher)
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+def create_game_sales(db: db_session, game_info: Publisher):
+    created_publisher = publisher_db.create_publisher(db, game_info)
+    return created_publisher
+
+
+@router.delete("/id/{id}")
+def delete_publisher_by_id(db: db_session, id: int):
+    deleted_publisher = publisher_db.delete_publisher_by_id(db, id)
+    return deleted_publisher
