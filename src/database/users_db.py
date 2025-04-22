79c6a3b47e22c78d3d0a6fb2b64e68fef78f9c83
Eric García <garciagomezeric10@gmail.com>
2025-04-22 13:58:42 +0200

    add delete user users_db

--- src/database/users_db.py
+++ src/database/users_db.py
@@ -8,3 +8,11 @@ def create_user(db: Session, user_info: User):
     db.commit()
     db.refresh(user_info)
     return user_info
+
+
+def delete_user_by_id(db: Session, id: int):
+    query = select(User).where(User.id == id)
+    result = db.exec(query).first()
+    db.delete(result)
+    db.commit()
+    return result
--- src/routes/users.py
+++ src/routes/users.py
@@ -11,3 +11,9 @@ router = APIRouter(prefix="/users", tags=["Users"])
 def create_user(db: db_session, user_info: User):
     created_user = users_db.create_user(db, user_info)
     return created_user
+
+
+@router.delete("/id/{id}")
+def delete_user_by_id(db: db_session, id: int):
+    deleted_user = users_db.delete_user_by_id(db, id)
+    return deleted_user
