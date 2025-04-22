94b5cdc2060beafc57103e6cab6a6373dea24f70
Eric García <garciagomezeric10@gmail.com>
2025-04-22 13:53:55 +0200

    add create users

--- src/routes/users.py
+++ src/routes/users.py
@@ -5,3 +5,9 @@ from src.database import db_session, users_db
 from src.models.users_model import User
 
 router = APIRouter(prefix="/users", tags=["Users"])
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+def create_user(db: db_session, user_info: User):
+    created_user = users_db.create_user(db, user_info)
+    return created_user
