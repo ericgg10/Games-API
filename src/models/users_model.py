6dd848e0177146072d9178e711ea209d798238fe
Eric García <garciagomezeric10@gmail.com>
2025-05-15 18:21:18 +0200

    fix error password value

--- src/models/users_model.py
+++ src/models/users_model.py
@@ -6,16 +6,16 @@ from sqlmodel import Field, SQLModel
 class User(SQLModel, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     name: str | None
-    password: str
+    password: int
     role: int
 
 
 class UserUpdate(SQLModel):
     id: uuid.UUID
     name: str | None
-    password: str
+    password: int
 
 
 class UserCreate(SQLModel):
     name: str | None
-    password: str
+    password: int
