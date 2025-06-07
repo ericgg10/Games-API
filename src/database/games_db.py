df755e55cc31927cab84e4441b8c9a857ea5b0bb
Eric García <garciagomezeric10@gmail.com>
2025-06-07 17:42:01 +0200

    add platform, genre and publisher names

--- src/database/games_db.py
+++ src/database/games_db.py
@@ -21,15 +21,27 @@ def get_games_by_field(
     limit: int = 10,
 ):
     query = (
-        select(Game)
+        select(Game, Platform.name, Genre.name, Publisher.name)
         .join(Publisher, Game.publisher_id == Publisher.id)
         .join(Genre, Game.genre_id == Genre.id)
         .join(Platform, Game.platform_id == Platform.id)
         .where(field_value == column)
         .limit(limit)
     )
-    result = db.exec(query)
-    return result.all()
+    result = db.exec(query).all()
+
+    list_of_result = []
+    for single_result in result:
+        game, platform_name, genre_name, publisher_name = single_result
+        game_public = GamePublic(
+            **game.model_dump(),
+            genre_name=genre_name,
+            platform_name=platform_name,
+            publisher_name=publisher_name,
+        )
+        list_of_result.append(game_public)
+
+    return list_of_result
 
 
 # GET GAMES FIELD
@@ -45,14 +57,26 @@ def get_games_field(db: Session, table: SQLModel):
 # GET GAMES BETWEEN
 def get_games_between(db: Session, column, field_1, field_2, limit):
     query = (
-        select(Game.name)
+        select(Game, Platform.name, Genre.name, Publisher.name)
         .join(GameSales, Game.gamesales_id == GameSales.id)
         .where(column >= field_1)
         .where(column <= field_2)
         .limit(limit)
     )
-    result = db.exec(query)
-    return result.all()
+    result = db.exec(query).all()
+
+    list_of_result = []
+    for single_result in result:
+        game, platform_name, genre_name, publisher_name = single_result
+        game_public = GamePublic(
+            **game.model_dump(),
+            genre_name=genre_name,
+            platform_name=platform_name,
+            publisher_name=publisher_name,
+        )
+        list_of_result.append(game_public)
+
+    return list_of_result
 
 
 # DELETE GAME
@@ -83,12 +107,6 @@ def update_game(db: Session, new_game: Game):
     db.commit()
     db.refresh(old_game)
     return old_game
-
-
-def get_all_games(db: Session, limit):
-    query = select(Game).limit(limit)
-    result = db.exec(query).all()
-    return result
 
 
 def get_public_game(db: Session, page_size, page_index):
